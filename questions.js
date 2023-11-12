f02b9110df6469fc62084c8275e16bdd45cb455e
usama <112273462+iamause@users.noreply.github.com>
2023-11-12 21:19:31 +0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Spot the fake one",
+    answer: "Alfred Williams was a scientist who invented the jetpack",
+    options: [
+      "Sergei Avdeyev time travelled 0.02 seconds into future",
+      "The bullet-proof vest was invented by a pizza delivery man",
+      "The electric chair was invented by a dentist",
+      "Alfred Williams was a scientist who invented the jetpack"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The largest desert is located in",
+    answer: "Antartica",
+    options: [
+      "Africa",
+      "United Arab Emirates",
+      "Dubai",
+      "Antartica"
+    ]
+  },
+    {
+    numb: 3,
+    question: "A periodic table contains",
+    answer: "All the discovered Elements",
+    options: [
+      "Food",
+      "All the discovered Elements",
+      "Compounds",
+      "Types of metals"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper tool making Language",
+      "Hyper Text Making Language",
+      "Hyper Text Markup Language",
+      "Hello Text Markup Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who is the richest person in the world",
+    answer: "Elon Musk",
+    options: [
+      "Bill gates",
+      "Mark Zuckerburg",
+      "Elon Muks",
+      "Elon Musk"
+    ]
+  },
+  
+
+    {
+    numb: 6,
+    question: "Who was the first President of Canada?",
+    answer: "Alexander Macdonald",
+    options: [
+      "Alexander Macdonald",
+      "Chaim Weizmann",
+      "Obama",
+      "Mujibur Rahman"
+    ]
+  },
+];
